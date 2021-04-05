779c4640f7b251419ac11c63d788ff7c6394e15e
Ataberk Kasap <c.ataberkkasap@hotmail.com>
2021-04-05 17:30:11 +0300

    Homework3 for day3

--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,13 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriID = 1;
+            musteri1.Isim = "Ataberk";
+            musteri1.Soyisim = "Kasap";
+
+            musteriManager.MusteriEkle(musteri1);
         }
     }
 }
