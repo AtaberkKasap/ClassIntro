2e499642b97df4bb3fba337884d41d204b58394a
Ataberk Kasap <c.ataberkkasap@hotmail.com>
2021-04-05 16:45:36 +0300

    First Push

---
+++ Metotlar/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+//using System.Collections.Generic;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<Urun> urunler = new List<Urun>();
+            //urunler.Add(new Urun());
+            //urunler[0].Adi = "Elma";
+            //foreach (var urun in urunler)
+            //{
+                //Console.WriteLine(urun.Adi);
+            //}
+
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Aciklama = "Amasya Elması";
+            urun1.Fiyati = 15;
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+            urun2.Fiyati = 80;
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine("-----------------");
+            }
+
+            Console.WriteLine("----------Metotlar----------");
+
+            //instance - örnek
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 12, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır Karpuzu", 12, 8);
+        }
+    }
+}
+
+
+// Don't repeat yourself - DRY - Clean Code - Best Practice
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //List<Urun> sepettekiUrunler = new List<Urun>();
+
+        // naming convention
+        // syntax
+        public void Ekle(Urun urun)
+        {
+            //sepettekiUrunler.Add(urun);
+            Console.WriteLine("Tebrikler. Sepete eklendi! : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi! : " + urunAdi);
+        }
+    }
+}
