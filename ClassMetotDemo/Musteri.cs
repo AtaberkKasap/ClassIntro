080ab0820a4d3dfb3228cc0e3171e1e93aecd289
Ataberk Kasap <c.ataberkkasap@hotmail.com>
2021-04-05 17:18:15 +0300

    Homework3 for day3

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public int MusteriID { get; set; }
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+    }
+}
