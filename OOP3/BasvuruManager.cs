f3e59f1c2ab74d1ad02fa6a7b436df364e8e92d4
Ataberk Kasap <c.ataberkkasap@hotmail.com>
2021-04-07 00:27:28 +0300

    End of day5 lesson.

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    // Method Injection
+    class BasvuruManager
+    {
+        public void BasvuruYap(IKrediManager krediManager, List<ILoggerService> loggerService)
+        {
+            // Başvuran bilgilerini değerlendirme
+            //
+            krediManager.Hesapla();
+            foreach (var logger in loggerService)
+            {
+                logger.Log();
+            }
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILoggerService loggerService = new DatabaseLoggerService();
+
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            List<ILoggerService> loggerServices = new List<ILoggerService> { new SmsLoggerService(), new DatabaseLoggerService() };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager, loggerServices);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
