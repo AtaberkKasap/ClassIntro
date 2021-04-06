c4d9289fb93b16321b296aa8108c8fdcb3c1c19b
Ataberk Kasap <c.ataberkkasap@hotmail.com>
2021-04-06 12:02:06 +0300

    Day 4 end of lesson.

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi");
+        }
+
+        public int Topla(int x, int y)
+        {
+            return x + y;
+        }
+
+        public void Topla2(int x, int y)
+        {
+            Console.WriteLine(x + y);
+        }
+    }
+}
